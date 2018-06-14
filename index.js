16283b6e826ccf75bb68068cc616034fba5701da
CubeRikkert <ricksamuel@gmail.com>
2018-06-14 14:43:45 +0200

    redux tests with arrays added

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const redux = require('redux')
 
-const initialState = {
-  firstName: 'Charles',
-  lastName: 'Eamnes',
-  age: 75
-}
+const initialState = [
+  {
+    name: 'First dog',
+    isAGoodBoy: true
+  }
+]
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
-  case 'SET_FIRST_NAME':
-    return {
-      ...state,
-      firstName: action.payload
-    }
-  case 'SET_LAST_NAME':
-    return {
-      ...state,
-      lastName: action.payload
-    }
-  case 'SET_AGE':
-    return {
-      ...state,
-      age: action.payload
-    }
-  default:
-    return state
+    case 'ADD_DOG':
+      return [
+        ...state,
+        action.payload
+      ]
+    case 'SET_DOGS':
+      return [
+        ...state,
+        action.payload
+      ]
+    default:
+      return state
   }
 }
 
@@ -34,27 +30,20 @@ console.log('Initial state of the store', store.getState())
 store.subscribe(() => console.log('Next state:', store.getState()))
 
 const action = {
-  type: 'SET_FIRST_NAME',
-  payload: 'Alice'
+  type: 'ADD_DOG',
+  payload: {
+    name: 'Second dog',
+    isAGoodBoy: false
+  }
 }
 
 store.dispatch(action)
 
 const actionTwo = {
-  type: 'SET_LAST_NAME',
-  payload: 'Bob'
+  type: 'SET_DOGS',
+  payload: []
 }
 
 store.dispatch(actionTwo)
-
-const actionThree = {
-  type: 'SET_AGE',
-  payload: 12345
-}
-
-store.dispatch(actionThree)
-
-
-
 
 module.exports = { reducer }
